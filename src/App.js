722676a44a5bdb3a8686ed79f38fcdc78a3c7902
paul172v <paul172v@aol.com>
2023-09-08 22:44:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classes from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className={classes.background}>
+        <div className={classes["header-desktop"]}>
+          <div className={classes["details-wrapper"]}>
+            <h1>Paul Harris</h1>
+            <h1>Frontend Web Developer (MERN Stack)</h1>
+            <p>Based in Glasgow</p>
+            <p>Looking for work</p>
+            <p>Willing to relocate</p>
+
+            <a href="https://github.com/paul172v">My GitHub</a>
+            <a href="mailto:paul172v@aol.com">Email Me</a>
+            <a href="tel:+447375384303">Call Me</a>
+          </div>
+        </div>
+
+        <div className={classes["header-mobile"]}>
+          <div className={classes["details-wrapper"]}>
+            <h1>Paul Harris</h1>
+            <h1>Frontend Web Developer (MERN Stack)</h1>
+            <p>Based in Glasgow</p>
+            <p>Looking for work</p>
+            <p>Willing to relocate</p>
+
+            <a href="https://github.com/paul172v">My GitHub</a>
+            <a href="mailto:paul172v@aol.com">Email Me</a>
+            <a href="tel:+447375384303">Call Me</a>
+          </div>
+        </div>
+      </div>
+      <video
+        className={classes.video}
+        src="/vids/background.mp4"
+        autoplay="true"
+        loop="true"
+      />
+    </React.Fragment>
   );
 }
 
